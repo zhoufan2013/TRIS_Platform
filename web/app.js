e18e7853efe84ab4de7a2c81c51e993ad2a2993d
zhoufan <scholes17@gmail.com>
2015-06-15 14:41:29 +0800

    commit initial

--- web/app.js
+++ web/app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var ejs = require('ejs');
 
 var app = express();
@@ -29,9 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
